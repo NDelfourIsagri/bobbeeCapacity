5e3e31634c9ebf16fea2d5b65e3b4ff8314b5dbe
Nicolas DELFOUR <ndelfour@isagri.fr>
2026-05-21 08:18:59 +0200

    rendre l'installation possible

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'bobbee-v1';
+const SHELL = [
+  '/bobbeeCapacity/',
+  '/bobbeeCapacity/assets/css/app.css',
+  '/bobbeeCapacity/assets/js/app.js',
+  '/bobbeeCapacity/assets/img/favicon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
